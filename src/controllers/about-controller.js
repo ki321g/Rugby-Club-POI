2c16d06abb50047703cbb240c7e08aef29b7e747
Kieron Garvey <garveykieron@gmail.com>
2024-03-01 09:16:28 +0000

    Fix for about-controller.js when rendering about-view when no loggedInUser

--- src/controllers/about-controller.js
+++ src/controllers/about-controller.js
@@ -5,14 +5,14 @@ export const aboutController = {
     },
     handler: function (request, h) {
       const loggedInUser = request.auth.credentials;
-      let UserLoggedIn = Boolean(loggedInUser);      
+      let UserLoggedIn = Boolean(loggedInUser);
       let superAdmin = false;
-
-      if (loggedInUser.accountType === "superadmin") {
-        superAdmin = Boolean(loggedInUser.accountType);
-        console.log("superAdmin: " + superAdmin);
+      if (loggedInUser) {
+        if (loggedInUser.accountType === "superadmin") {
+          superAdmin = Boolean(loggedInUser.accountType);
+          console.log("superAdmin: " + superAdmin);
+        }
       }
-
       const viewData = {
         title: "About RugbyGamePOI",
         user: request.auth.credentials,
