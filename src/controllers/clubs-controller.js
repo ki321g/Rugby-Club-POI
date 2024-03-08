99883c46aea03759d9c96c5fcf1dd45e8f2e95f1
Kieron Garvey <garveykieron@gmail.com>
2024-03-08 12:17:39 +0000

    fix home page ui and club for none logged in user

--- src/controllers/clubs-controller.js
+++ src/controllers/clubs-controller.js
@@ -4,48 +4,82 @@ import { db } from "../models/db.js";
 
 
 export const clubsController = {
-index: {
+  index: {
+    auth: {
+      mode: "try",
+    },
     handler: async function (request, h) {      
       const loggedInUser = request.auth.credentials;
       let superAdmin = false;
       let hideAddClub = false;
+      let UserLoggedIn = false;
       let checkClubs, numberClubs;
-
-      const userClubs = await db.clubStore.getUserClubs(loggedInUser._id);
-      if (loggedInUser.accountType === "superadmin" || loggedInUser.accountType === "admin") {
-        superAdmin = Boolean(loggedInUser.accountType);
-      }
-      checkClubs = userClubs.sort((a, b) => a.club.localeCompare(b.club));
-      numberClubs = checkClubs.length;
       
-      let UserLoggedIn = Boolean(loggedInUser);
+      if (loggedInUser) {
+        const userClubs = await db.clubStore.getUserClubs(loggedInUser._id);
+        if (loggedInUser.accountType === "superadmin" || loggedInUser.accountType === "admin") {
+          superAdmin = Boolean(loggedInUser.accountType);
+        }
+        checkClubs = userClubs.sort((a, b) => a.club.localeCompare(b.club));
+        numberClubs = checkClubs.length;
+        
+        UserLoggedIn = Boolean(loggedInUser);
 
-      if (numberClubs > 0) {
-        hideAddClub = true;
-      }
+        if (numberClubs > 0) {
+          hideAddClub = true;
+        }
+      } 
 
-    const clubs = await db.clubStore.getAllClubs();
-    const viewData = {
-        title: "List Clubs",
-        user: loggedInUser,
-        superAdmin: superAdmin,
-        clubs: clubs,
-        UserLoggedIn: UserLoggedIn,
-        hideAddClub: hideAddClub,
-        clubs,
+      const clubs = await db.clubStore.getAllClubs();
+      const viewData = {
+          title: "List Clubs",
+          user: loggedInUser,
+          superAdmin: superAdmin,
+          clubs: clubs,
+          UserLoggedIn: UserLoggedIn,
+          hideAddClub: hideAddClub,
+          clubs,
     };
     return h.view("clubs-view", viewData);
     },
-},
+  },
   category: {
+    auth: {
+      mode: "try",
+    },
     handler: async function (request, h) {
       const loggedInUser = request.auth.credentials;
       const category = request.params.category.toUpperCase();
       const clubs = await db.clubStore.getClubsByCategory(category);
       
+      let superAdmin = false;
+      let hideAddClub = false;
+      let UserLoggedIn = false;
+      let checkClubs, numberClubs;
+      
+      if (loggedInUser) {
+        
+        const userClubs = await db.clubStore.getUserClubs(loggedInUser._id);
+        if (loggedInUser.accountType === "superadmin" || loggedInUser.accountType === "admin") {
+          superAdmin = Boolean(loggedInUser.accountType);
+        }
+        checkClubs = userClubs.sort((a, b) => a.club.localeCompare(b.club));
+        numberClubs = checkClubs.length;
+        
+        UserLoggedIn = Boolean(loggedInUser);
+
+        if (numberClubs > 0) {
+          hideAddClub = true;
+        }
+      } 
+
       const viewData = {
         title: "List Club Results",
         user: loggedInUser,
+        superAdmin: superAdmin,
+        clubs: clubs,
+        UserLoggedIn: UserLoggedIn,
+        hideAddClub: hideAddClub,
         clubs,
       };
       return h.view("clubs-view", viewData);
